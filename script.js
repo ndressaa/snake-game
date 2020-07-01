4ab02e95700fd5be2a490687df7261ccecaa6de9
ANDRESSA <andressajucoski@gmail.com>
2020-07-01 16:35:17 -0300

    game over setting

--- script.js
+++ script.js
@@ -44,6 +44,13 @@ function startGame() {
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i=1; i<snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(game);
+            alert('Game Over !');
+        }
+    }
+
     createBG();
     createSnake();
     drawFood();
@@ -72,4 +79,4 @@ function startGame() {
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(startGame, 100);
+let game = setInterval(startGame, 100);
